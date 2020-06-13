fb684adfda2f22b2bd40dfbbf51bb005c7a4f406
ntkchinh <ngokieuchinh89@gmail.com>
2020-06-14 00:20:41 +0700

    Update dot_product_project2d.js
    'var' to 'let';
    replace scatter[*] with u, v;
    changing var name (projection_line, dot_product..);
    plot uTvv;

--- assets/js/linear_algebra/dot_product_project2d.js
+++ assets/js/linear_algebra/dot_product_project2d.js
@@ -1,28 +1,25 @@
-var dot_product_project2d = (function() {
+let dot_product_project2d = (function() {
 
+let origin = [150, 140], 
+    scale = 60, 
+    scatter = [], 
+    axis = [],
+    expectedAxis = [],
 
+    startAngleX = Math.PI,
+    startAngleY = 0.,
+    startAngleZ = 0.,
+    
+    axis_len = 2,
+    unit = axis_len/10,
+    
+    svg = d3.select("#svg_dot_product_project2d");
 
-var origin = [150, 140], 
-  scale = 60, 
-  scatter = [], 
-  axis = [],
-  expectedAxis = [],
-  startAngleX = Math.PI,
-  startAngleY = 0.,
-  startAngleZ = 0.;
-let  axis_len = 2;
-
-let unit = axis_len/10;
-
-var svg = d3.select("#svg_dot_product_project2d");
-
-
-var lib = space_plot_lib(
+let lib = space_plot_lib(
   svg,
   origin, 
   scale,
   is_2d=true);
-
 
 svg = svg.call(d3.drag()
          .on('drag', dragged)
@@ -32,8 +29,9 @@ svg = svg.call(d3.drag()
 
 axis = lib.init_float_axis(axis_len=axis_len, unit=unit);
 
-
 function plot(scatter, axis, tt){
+  let u = scatter[0],
+      v = scatter[1];
 
   basis = {
       ex: axis[1/unit - 1 + axis_len * 0][1], 
@@ -42,7 +40,7 @@ function plot(scatter, axis, tt){
   };
 
   scatter.forEach(function(d, i){
-    var coord = lib.dot_basis(d, basis);
+    let coord = lib.dot_basis(d, basis);
     if (i == 0) {
       d.text = 'u = ';
     } else {
@@ -60,8 +58,8 @@ function plot(scatter, axis, tt){
 
   lib.plot_lines(axis);
 
-  var lines = [];
-  scatter.forEach(function(d,i){
+  let lines = [];
+  scatter.forEach(function(d, i){
     lines.push([
         {x: 0., y: 0., z: 0.},
         {x: d.x, y: d.y, z: d.z, 
@@ -72,26 +70,36 @@ function plot(scatter, axis, tt){
     }
   });
 
-  let dot_product = scatter[0].x * scatter[1].x +
-                 scatter[0].y * scatter[1].y;
-  let projectionx = scatter[1].x * dot_product;
-  let projectiony = scatter[1].y * dot_product;
+  let uTv = lib.dot_product(u, v);
+  let points = [];
+  scatter.forEach(function(d){
+    points.push(d)
+  })
+  let uTvv = {
+      x: v.x * uTv,
+      y: v.y * uTv,
+      z: v.z * uTv,
+      r: 1.8,
+      color: 'grey'
+  };
+  uTvv.centroid_z = 1000;
+  points.push(uTvv);
 
-  let projection_line = [
+  let uTvv_line = [
       {x: 0, y: 0, z: 0},
-      {x: projectionx, y: projectiony, z: 0,
-       color: 0, tt:true}
+      {x: uTvv.x, y: uTvv.y, z: 0,
+       tt:true}
   ];
+  uTvv_line.color = 0;
+  uTvv_line.text = 'u\u1d40v = '.concat(uTv.toFixed(3));
+  uTvv_line.text_color = 0;
+  uTvv_line.font_size = 15;
 
-  projection_line.text = 'u\u1d40v = '.concat(dot_product.toFixed(3));
-  projection_line.text_color = 0;
-  projection_line.font_size = 15;
-
-  lines.push(projection_line);
+  lines.push(uTvv_line);
 
   let dash_line = [
-      {x: scatter[0].x, y: scatter[0].y, z: scatter[0].z},
-      {x: projectionx, y: projectiony, z: 0,
+      {x: u.x, y: u.y, z: u.z},
+      {x: uTvv.x, y: uTvv.y, z: 0,
        tt:true}];
   
   dash_line.dash = true;
@@ -99,7 +107,7 @@ function plot(scatter, axis, tt){
   lines.push(dash_line);
 
   lib.plot_lines(lines, tt, 'arrow');
-  lib.plot_points(scatter, tt,
+  lib.plot_points(points, tt,
                     drag_point_fn=function(d, i){
                       if (i == 0) {
                         dragged_point(i);
@@ -112,36 +120,30 @@ function plot(scatter, axis, tt){
   lib.sort();
 }
 
-
 function init(){
-  scatter = [];
+  let u = {
+      x: -1.0,
+      y: -4.0/3, 
+      z: 0.,
+      color: 4,
+  };
 
-  scatter.push({
-    x: -1.0,
-    y: -4.0/3, 
-    z: 0.,
-    color: 4,
-  });
+  let v = {
+      x: 1/Math.sqrt(3),
+      y: -Math.sqrt(2/3), 
+      z: 0.,
+      color: 2,
+  };
 
-  scatter.push({
-    x: 1/Math.sqrt(3),
-    y: -Math.sqrt(2/3), 
-    z: 0.,
-    color: 2,
-  })
+  scatter = [u, v]
 
-
-  alpha = startAngleX;
-  beta = startAngleY;
-
-  expectedScatter = lib.rotate_points(scatter, alpha, beta, startAngleZ);
-  expectedAxis = lib.rotate_lines(axis, alpha, beta, startAngleZ);
+  expectedScatter = lib.rotate_points(scatter, startAngleX, startAngleY, startAngleZ);
+  expectedAxis = lib.rotate_lines(axis, startAngleX, startAngleY, startAngleZ);
   plot(expectedScatter, 
        expectedAxis, 
        1000);
   drag_end();
 }
-
 
 function drag_start(){
   lib.drag_start2d();
@@ -183,7 +185,6 @@ function dragged_point(i){
        expectedAxis, 
        0);
 }
-
 
 function drag_end(){
   scatter = expectedScatter;
